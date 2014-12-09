731a27c52acbf5d687ac973e47e9e3d54fda119a
abhaysinghCMU <abhays@andrew.cmu.edu>
2014-12-08 19:54:30 -0500

    Added Xamarin library for Stripe API processing and created successful payment mechanism

--- Moviq/Api/CheckoutModule.cs
+++ Moviq/Api/CheckoutModule.cs
@@ -11,13 +11,49 @@ namespace Moviq.Api
     using System;
     using System.Collections;
     using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Xamarin.Payments.Stripe;
 
     public class CheckoutModule : NancyModule
     {
-
         public CheckoutModule(ICartItemDomain cartItems, IProductDomain products, IModuleHelpers helper)
         {
-     
+            this.Post["/api/checkout/pay/{cart_id}/{card_number}/{cvc}/{exp_month}/{exp_year}"] = args =>
+            {
+                // build credit card model
+                StripeCreditCardInfo cc = new StripeCreditCardInfo();
+                cc.Number = args.card_number;
+                cc.CVC = args.cvc;
+                cc.ExpirationMonth = args.exp_month;
+                cc.ExpirationYear = args.exp_year;
+
+                // calculate total cost of items
+                List<ICartItem> cartList = cartItems.Repo.GetCartItems(args.cart_id);
+                int cartCount = cartList==null?0:cartList.Count;
+                int totalCost = 0;
+                for (int i = 0; i < cartCount; i++)
+                {
+                    totalCost += Convert.ToInt32(cartList[i].Price * 100) * cartList[i].Quantity;
+                }
+
+                // charge credit card
+                if (totalCost > 0)
+                {
+                    System.Diagnostics.Debug.WriteLine("cost: " + totalCost);
+                    StripePayment payment = new StripePayment("sk_test_S9tiUYvwqfDyIhFWS7VReNzF");
+                    StripeCharge charge = payment.Charge(totalCost, "usd", cc, "Test charge");
+                    System.Diagnostics.Debug.WriteLine(charge);
+                }
+
+                // mark cart items as purchased using datetime stamp
+                for (int i = 0; i < cartCount; i++)
+                {
+                    cartList[i].PurchaseDate = DateTime.Now;
+                    cartItems.Repo.Set(cartList[i]);
+                }
+
+                return "";
+            };
         }
     }
 }
--- Moviq/Js/controllers/checkoutController.js
+++ Moviq/Js/controllers/checkoutController.js
@@ -10,6 +10,66 @@ define('controllers/checkoutController', {
             self.template = template;
             self.data = data;
             self.after = function () {
+                // populate year field with reasonable values
+                var select = $(".card-expiry-year");
+                var year = new Date().getFullYear();
+                for (var i = 0; i < 12; i++) {
+                    select.append($("<option value='" + (i + year) + "' " + (i === 0 ? "selected" : "") + ">" + (i + year) + "</option>"))
+                }
+
+                // set stripe public key
+                Stripe.setPublishableKey('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
+            };
+
+            self.stripeResponseHandler = function (status, response) {
+                var $form = $('#payment-form');
+
+                if (response.error) {
+                    // Show the errors on the form
+                    $form.find('.payment-errors').text(response.error.message);
+                   // $form.find(':input:disabled').prop('disabled', false);
+                    $form.find(':input:disabled').prop('disabled', false);
+                   // $form.find('button').prop('disabled', false);
+                } else {
+                    // response contains id and card, which contains additional card details
+                    var card_number = $('.card-number').val();
+                    var cvc = $('.card-cvc').val();
+                    var exp_month = $('.card-expiry-month').val();
+                    var exp_year =  $('.card-expiry-year').val();
+                    var cart_id = $.cookie('cart_id');
+
+                    if (cart_id !== undefined) {
+                        // process token with our middleware
+                        $.ajax({
+                            url: '/api/checkout/pay/' + cart_id + '/' + card_number + '/' + cvc + '/' + exp_month + '/' + exp_year,
+                            method: 'POST'
+                        }).done(function (data) {
+                            alert('here');
+                            $form.find(':input:disabled').prop('disabled', false);
+                        });
+                    }
+
+
+                    // Insert the token into the form so it gets submitted to the server
+                    //$form.append($('<input type="hidden" name="stripeToken" />').val(token));
+                    // and submit
+                    //$form.get(0).submit();
+                }
+                
+
+                //alert('here: ' + status);
+            };
+
+            self.pay = function () {
+                var $form = $("#payment-form");
+                $form.find('.payment-errors').empty();
+
+                // Disable the submit form to prevent repeated clicks
+                $form.find(':input:not(:disabled)').prop('disabled', true)
+
+
+                Stripe.card.createToken($form, self.stripeResponseHandler);
+                
             };
 
             return self;
@@ -17,22 +77,33 @@ define('controllers/checkoutController', {
 
         // GET /#/checkout
         routes.get(/^\/#\/checkout\/?/i, function (context) {
-            $.ajax({
-                url: '/api/cart/list',
-                method: 'GET'
-            }).done(function (data) {
-                var results = new CartItems(JSON.parse(data));
+            var cart_id = $.cookie('cart_id');
 
-                if (results.cartItems().length > 0) {
-                    var checkoutViewModel = CheckoutViewModel('t-checkout', results);
-                    viewEngine.setView(checkoutViewModel);
-                } else {
-                    viewEngine.setView({
-                        template: 't-checkout',
-                        data: { searchterm: 'blah' }
-                    });
-                }
-            });
+            if (cart_id !== undefined)
+            {
+                $.ajax({
+                    url: '/api/cart/list/' + cart_id,
+                    method: 'GET'
+                }).done(function (data) {
+                    var results = new CartItems(JSON.parse(data));
+
+                    if (results.cartItems().length > 0) {
+                        var viewModel = CheckoutViewModel('t-checkout', results);
+                        viewEngine.setView(viewModel);
+                    } else {
+                        viewEngine.setView({
+                            template: 't-empty',
+                            data: { searchterm: 'blah' }
+                        });
+                    }
+                });
+
+            } else {
+                viewEngine.setView({
+                    template: 't-empty',
+                    data: { searchterm: 'blah' }
+                });
+            }
         });
     }
 });
